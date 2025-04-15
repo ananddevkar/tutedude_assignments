88c4b9c38c2b9633ac66bf123d0d1600e4058ddc
ananddevkar <addsbsindia@gmail.com>
2025-04-15 17:18:10 +0000

    commit

--- ASSIGNMENT 4: Task 1_2.py
+++ ASSIGNMENT 4: Task 1_2.py
@@ -1,9 +1,9 @@
-file = open('sample.txt' 'r+')
-writing_file = file.write('This is a simple text file\nit contains multiple files' 'r+')
+file1 = open('sample.txt' 'r+')
+writing_file = file1.write('This is a simple text file\nit contains multiple files' 'r+')
 print(writing_file)
 
-file = open('sample.txt' 'r+')
-reading_file = file.read('This is a simple text file\nit contains multiple files' 'r+')
+file1 = open('sample.txt' 'r+')
+reading_file = file1.read('This is a simple text file\nit contains multiple files' 'r+')
 print(reading_file)
 
 file.close()
